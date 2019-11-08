d9cde97436db1538bfb6348d88102c0027b55bc2
Thomas Beckford <tebeckford@gmail.com>
2019-11-08 15:31:50 -0300

    sd

---
+++ build/precache-manifest.af1745337e90b5f826def619ff6a0a17.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66ddd2cf4447f21e112a466bb739fc49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93139c907772f23d1a34",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "aad79c418099bf3e7c0c",
+    "url": "/static/css/main.ba781073.chunk.css"
+  },
+  {
+    "revision": "93139c907772f23d1a34",
+    "url": "/static/js/2.b69ff422.chunk.js"
+  },
+  {
+    "revision": "aad79c418099bf3e7c0c",
+    "url": "/static/js/main.7bc44272.chunk.js"
+  },
+  {
+    "revision": "c90a524f4f0b75534a45",
+    "url": "/static/js/runtime-main.c8f85229.js"
+  },
+  {
+    "revision": "b64453ddabf2172a7f5295e9a7ebf025",
+    "url": "/static/media/Julia.b64453dd.jpg"
+  },
+  {
+    "revision": "62936477ccd96aac66a8e84a85f5f765",
+    "url": "/static/media/Miguel.62936477.jpg"
+  }
+]);
