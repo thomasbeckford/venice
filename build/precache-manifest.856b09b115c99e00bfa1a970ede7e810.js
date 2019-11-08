e2e442a662c6b751964ff16bf172453ad81f502b
Thomas Beckford <tebeckford@gmail.com>
2019-11-08 15:52:56 -0300

    instagram

---
+++ build/precache-manifest.856b09b115c99e00bfa1a970ede7e810.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0c9406a5c7d26a4e1cfce5deb500b4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93139c907772f23d1a34",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "c002232106ebb9ad3dcd",
+    "url": "/static/css/main.ba781073.chunk.css"
+  },
+  {
+    "revision": "93139c907772f23d1a34",
+    "url": "/static/js/2.b69ff422.chunk.js"
+  },
+  {
+    "revision": "c002232106ebb9ad3dcd",
+    "url": "/static/js/main.4578a7ab.chunk.js"
+  },
+  {
+    "revision": "c90a524f4f0b75534a45",
+    "url": "/static/js/runtime-main.c8f85229.js"
+  },
+  {
+    "revision": "b64453ddabf2172a7f5295e9a7ebf025",
+    "url": "/static/media/Julia.b64453dd.jpg"
+  },
+  {
+    "revision": "62936477ccd96aac66a8e84a85f5f765",
+    "url": "/static/media/Miguel.62936477.jpg"
+  }
+]);
